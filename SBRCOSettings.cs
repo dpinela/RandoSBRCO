0d88537b5ac2e230f3463601af5e616ea7c90634
Diogo Pinela <diogoid7400@gmail.com>
2024-02-29 02:40:32 +0000

    Fix bug that caused a random subset of SBRCodes to not be accepted

--- SBRCOSettings.cs
+++ SBRCOSettings.cs
@@ -23,7 +23,16 @@ public class SBRCOSettings
             44 => Data.VanillaCharmOrder.Concat(Data.TranscendenceCharmOrder).ToArray(),
             _ => throw new ArgumentException($"Invalid SBRCode length; is {SBRCode.Length}, should be either 28 or 44")
         };
-        return Data.KthPermutation(allCharms.Length, new BigInteger(Convert.FromBase64String(SBRCode).Reverse().ToArray()))
+        var rawBytes = Convert.FromBase64String(SBRCode);
+        // It is not sufficient to reverse the rawBytes; we must add
+        // a zero at the end so that the BigInteger constructor is
+        // guaranteed to produce a positive number.
+        var bytes = new byte[rawBytes.Length + 1];
+        for (var i = 0; i < rawBytes.Length; i++)
+        {
+            bytes[rawBytes.Length - 1 - i] = rawBytes[i];
+        }
+        return Data.KthPermutation(allCharms.Length, new BigInteger(bytes))
             .ToList();
     }
 }
